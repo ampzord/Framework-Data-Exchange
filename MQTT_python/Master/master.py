28b94086ba85d1610d2b3cc7b3e838828460b6fb
António Pereira <antonio_m_sp@hotmail.com>
2021-07-15 17:29:51 +0100

    Restructured Master code and readability.

--- MQTT_python/Master/master.py
+++ MQTT_python/Master/master.py
@@ -2,9 +2,6 @@ import paho.mqtt.client as mqtt
 import time
 from influxdb import InfluxDBClient
 
-db = InfluxDBClient('localhost', 8086, 'root', 'root', 'master_db')
-db.create_database('master_db')
-
 
 def on_connect(client, userdata, flags, rc):
     if rc != 0:
@@ -28,42 +25,50 @@ def on_message(client, userdata, message):
         client.publish("topic/master", "Master received all data from Client 2.")
 
 
+def mqtt_init(tmp_master):
+    tmp_master.on_connect = on_connect
+    tmp_master.on_message = on_message
+    broker_address = "broker.hivemq.com"  # broker_address = "localhost"
+
+    tmp_master.connect(broker_address, port=1883)  # connect to broker
+
+
 def get_db_data():
     data = db.query("SELECT * FROM weldingEvents;")
-    # print("data: ", data)
+    print("data: ", data)
     points = data.get_points(tags={'measurement': 'weldingEvents'})
     print("Master DB: \n", data.raw)
     for point in points:
         print("Time: {}, Welding value: {}".format(point['time'], point['welding_value']))
 
-    '''
     try:
         new_data = db.query("SELECT count(welding_value) FROM weldingEvents;")
         all_events = list(new_data.get_points(measurement='weldingEvents'))
         print('Total Welding value count: ', all_events[0]['count'])
     except "CountError":
         print("Error counting number of total welding values reached Master DB.")
-    '''
 
 
-broker_address = "broker.hivemq.com"  # broker_address = "localhost"
+db = InfluxDBClient('localhost', 8086, 'root', 'root', 'master_db')
+db.create_database('master_db')
+
 master = mqtt.Client()  # create new instance
-master.on_connect = on_connect
-master.on_message = on_message
-master.connect(broker_address, port=1883)  # connect to broker
+mqtt_init(master)
 master.loop_start()
 
-print("Waiting 4 seconds...\n")
-time.sleep(5)
+print("Waiting 10 seconds for client be able to produce data...\n")
+time.sleep(10)
 
 master.publish("topic/master", "GET_INFORMATION")
 
 #################################################
 
-time.sleep(5)  # wait
+print("Waiting 10 seconds...\n")
+time.sleep(10)
+
 get_db_data()
 print('List of DBs: ', db.get_list_database())
 
-db.drop_database('master_db')
+# db.drop_database('master_db')
 master.loop_stop()  # stop the loop
 master.disconnect()
