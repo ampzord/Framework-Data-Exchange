f2cc30fa76cd8a952e8ce01cc5eee87e53fc06de
António Pereira <antonio_m_sp@hotmail.com>
2021-07-16 18:38:26 +0100

    Implemented time elapsed metric since request_info.

---
+++ MQTT_python/Client1/clients.bat
@@ -0,0 +1,3 @@
+FOR /L %%A IN (1,1,10) DO (
+  start py.exe "C:\Users\Amp-laptop\OneDrive\Documentos\FEUP\Tese\MQTT_Project\MQTT_python\Client1\client1.py" %%A
+)
--- client_script6.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\Python39\python.exe" "C:\Users\Amp-laptop\Documents\FEUP\Tese\MQTT_Project\MQTT_python\Client1\client1.py 6"
